ffaba79026f3d7c47f6570e0c2d3a7a3da6af396
Diego Morales <mor21146@uvg.edu.gt>
2022-09-04 21:41:29 -0600

    Comentarios

--- Parte 1/parte2_mod.cpp
+++ Parte 1/parte2_mod.cpp
@@ -12,16 +12,16 @@
 #include <string.h>
 #include <iostream>
 
-pthread_mutex_t mutex;
+pthread_mutex_t mutex; //Mutex para el acceso a la sección crítica
 float total = 0;
 void *serie(void *arg){
     float resultado = 0.0; 
     float *n = (float *)arg;
     float n_sum = *n; 
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&mutex); //Bloquea el mutex
     resultado = 1/((n_sum)*(n_sum+1));
     total = total + resultado;
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&mutex); //Desbloquea el mutex
     printf("Cuando n es: %.0f el resultado es: %.5f\n", *n, total);  
 
 }
@@ -30,23 +30,23 @@ int main(void) {
     int valor_n; 
     pthread_t threads;
     pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    pthread_mutex_init(&mutex, NULL);
+    pthread_attr_init(&attr); //Inicializa los atributos del thread
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); //Hace que el thread sea joinable
+    pthread_mutex_init(&mutex, NULL); //Inicializa el mutex
     printf("Ingrese el numero maximo: ");
     scanf("%d", &valor_n);
     //imprime valor n
     printf("Valor n: %d\n", valor_n);
     int i;
-    for(i = 1; i <= valor_n; i++){
-        float j = (float)i;
-        pthread_create(&threads, &attr, serie, (void *)&j);
-        pthread_join(threads, NULL);
+    for(i = 1; i <= valor_n; i++){  
+        float j = (float)i; 
+        pthread_create(&threads, &attr, serie, (void *)&j); //Crea el thread
+        pthread_join(threads, NULL); //Espera a que el thread termine
         
     }
     printf("El resultado de la serie es: %.5f\n", total);
-    pthread_attr_destroy(&attr);
-	pthread_exit(NULL);
-    pthread_mutex_destroy(&mutex);
+    pthread_attr_destroy(&attr); //Destruye los atributos del thread
+	pthread_exit(NULL); //Termina el thread
+    pthread_mutex_destroy(&mutex); //Destruye el mutex
     return 0;
 }
